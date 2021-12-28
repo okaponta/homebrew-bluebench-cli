340bc315b42e318f227acfbba6f4ee27338d075b
Kohei Okamoto <okaponta.19910919@gmail.com>
2021-12-28 20:10:13 +0900

    Revert "update"
    This reverts commit 48732782dccbb6148b9c0dc29056bfa4fe298be0.

--- Formula/bluebench-cli.rb
+++ Formula/bluebench-cli.rb
@@ -1,20 +1,11 @@
 class BluebenchCli < Formula
   desc "青いベンチ診断をローカルで実行します"
   homepage "https://okaponta.github.io/profile/"
-  url "https://github.com/okaponta/bluebench-cli/archive/refs/tags/v1.2.0.tar.gz"
-  sha256 "7e23667d97c0e967d981db04839ab539d2879ed6b8c0f2cf89a57e4c6537c5ef"
+  url "https://github.com/okaponta/bluebench-cli/releases/download/v1.2.0/bluebench-cli"
+  sha256 "ad7c61fe88d4e06e695a6889dc9d801d69089d547ae0c51f53400e10acbac25e"
   license "Apache-2.0"
 
   def install
-    print buildpath
-    ENV["GOPATH"] = buildpath
-    install_path = buildpath/"src/github.com/okaponta/bluebench-cli/"
-    install_path.install buildpath.children
-
-    cd install_path do
-      system "dep", "ensure", "-vendor-only"
-      system "go", "build"
-      bin.install "bluebench-cli"
-    end
+    bin.install "bluebench-cli"
   end
 end
